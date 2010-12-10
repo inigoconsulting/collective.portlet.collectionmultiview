7f212c1f17c5f2368d7b9ef9d617d47cfc4cb83f
Izhar Firdaus <kagesenshi.87@gmail.com>
2010-12-10 04:56:23 +0000

    collectionmultiview:  * code cleanup
    git-svn-id: http://svn.plone.org/svn/collective/collective.portlet.collectionmultiview/trunk@228637 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- src/collective/portlet/collectionmultiview/collectionmultiview.py
+++ src/collective/portlet/collectionmultiview/collectionmultiview.py
@@ -35,21 +35,7 @@ alsoProvides(RendererVocabulary,IVocabularyFactory)
 
 
 class ICollectionMultiView(collection.ICollectionPortlet):
-    """A portlet
 
-    It inherits from IPortletDataProvider because for this portlet, the
-    data that is being rendered and the portlet assignment itself are the
-    same.
-    """
-
-    # TODO: Add any zope.schema fields here to capture portlet configuration
-    # information. Alternatively, if there are no settings, leave this as an
-    # empty interface - see also notes around the add form and edit form
-    # below.
-
-    # some_field = schema.TextLine(title=_(u"Some field"),
-    #                              description=_(u"A field to use"),
-    #                              required=True)
     renderer = schema.Choice(title=_(u'Renderer'),
                          description=_(u"The name of the Renderer for this portlet."),
                          default='default',
@@ -58,34 +44,18 @@ class ICollectionMultiView(collection.ICollectionPortlet):
 
 
 class Assignment(collection.Assignment):
-    """Portlet assignment.
-
-    This is what is actually managed through the portlets UI and associated
-    with columns.
-    """
 
     implements(ICollectionMultiView)
 
-    # TODO: Set default values for the configurable parameters here
-
-    # some_field = u""
-
-    # TODO: Add keyword parameters for configurable parameters here
-    # def __init__(self, some_field=u""):
-    #    self.some_field = some_field
-
-    def __init__(self, header=u"", target_collection=None, limit=None, random=False, show_more=True, show_dates=False,renderer='default'):
-        super(Assignment,self).__init__(header,target_collection,limit,random,show_more,show_dates)
+    def __init__(self, header=u"", target_collection=None, limit=None,
+                 random=False, show_more=True, show_dates=False, 
+                 renderer='default'):
+        super(Assignment,self).__init__(header, target_collection, limit, 
+                                        random, show_more, show_dates)
         self.renderer = renderer
 
 
 class Renderer(collection.Renderer):
-    """Portlet renderer.
-
-    This is registered in configure.zcml. The referenced page template is
-    rendered, and the implicit variable 'view' will refer to an instance
-    of this class. Other methods can be added and referenced in the template.
-    """
     implements(ICollectionMultiViewBaseRenderer)
 
     @property
@@ -94,46 +64,28 @@ class Renderer(collection.Renderer):
         if renderer is None:
            self.data.renderer = 'default'
            renderer = 'default'
-        return getAdapter(self,ICollectionMultiViewRenderer,renderer).render
+        return getAdapter(self, ICollectionMultiViewRenderer, renderer).render
 
 
 class AddForm(collection.AddForm):
-    """Portlet add form.
 
-    This is registered in configure.zcml. The form_fields variable tells
-    zope.formlib which fields to display. The create() method actually
-    constructs the assignment that is being added.
-    """
     form_fields = form.Fields(ICollectionMultiView)
     form_fields['target_collection'].custom_widget = UberSelectionWidget
+    # hide these fields from collectionportlet, we dont need these here
     form_fields = form_fields.omit('random', 'show_more', 'show_dates')
 
-    def create(self, data):
-        return Assignment(**data)
-
-
-# NOTE: If this portlet does not have any configurable parameters, you
-# can use the next AddForm implementation instead of the previous.
-
-# class AddForm(base.NullAddForm):
-#     """Portlet add form.
-#     """
-#     def create(self):
-#         return Assignment()
-
-
-# NOTE: If this portlet does not have any configurable parameters, you
-# can remove the EditForm class definition and delete the editview
-# attribute from the <plone:portlet /> registration in configure.zcml
-
+    label = _(u'Add CollectionMultiView portlet')
+    description = _(u"This portlet display a listing of items from a" + 
+                        " Collection, using custom views")
 
 class EditForm(collection.EditForm):
-    """Portlet edit form.
 
-    This is registered with configure.zcml. The form_fields variable tells
-    zope.formlib which fields to display.
-    """
     form_fields = form.Fields(ICollectionMultiView)
     form_fields['target_collection'].custom_widget = UberSelectionWidget
+    # hide these fields from collectionportlet, we dont need these here
     form_fields = form_fields.omit('random', 'show_more', 'show_dates')
+
+    label = _(u'Edit CollectionMultiView portlet')
+    description = _(u"This portlet display a listing of items from a" +
+                        " Collection, using custom views")
 
