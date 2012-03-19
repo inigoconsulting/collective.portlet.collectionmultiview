cd594d4c6c52693dd8494730093f53e4393c99d8
Izhar Firdaus <izhar@inigo-tech.com>
2012-03-19 22:40:57 +0800

    rename README.txt to README.rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.1.2'
 setup(name='collective.portlet.collectionmultiview',
       version=version,
       description="A collection portlet product which supports multiple views",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
